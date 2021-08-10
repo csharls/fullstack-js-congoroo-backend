1380a8bc37ed0d2cf147e9d9267cfae9ca5f5bc2
Carlos Garcia <carlossgarcia@outlook.com>
2021-08-10 07:27:14 -0600

    add phonebook backend step 10 and 11

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 
 
