8c3aee5cb8f03d058a25bc39c00927bc61df9fdf
Carlos Garcia <carlossgarcia@outlook.com>
2021-08-11 05:16:22 -0600

    Add debug to browser nodejs

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ app.get('/', (req,res)=> {
 app.get('/info',(req, res)=>{
   const total = persons.length
   const date = new Date()
-  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', timeZone: 'UTC', timeZoneName:'long'};
   res.send(`
   <p>Phonebook has info for ${total} people</p>
   <p>${date}</p>`)
