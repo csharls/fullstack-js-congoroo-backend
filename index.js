ec3b8e823f7b8292d9eb514dbfc2742a85fb528c
Carlos Garcia <carlossgarcia@outlook.com>
2021-08-20 06:28:01 -0600

    phonebook database step 3

--- index.js
+++ index.js
@@ -5,37 +5,11 @@ const Person = require('./models/person')
 
 const app = new express()
 
-  // let persons=[
-  //   {
-  //     "name": "Arto Hellas",
-  //     "number": "040-134567",
-  //     "id": 1
-  //   },
-  //   {
-  //     "name": "Ada Lovelace",
-  //     "number": "39-44-5323523",
-  //     "id": 2
-  //   },
-  //   {
-  //     "name": "Dan Abramov",
-  //     "number": "12-43-234345",
-  //     "id": 3
-  //   },
-  //   {
-  //     "name": "Mary Poppendieck",
-  //     "number": "39-23-6423122",
-  //     "id": 4
-  //   }
-  // ]
-
-
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
-
-
 
 app.get('/', (req,res)=> {
   res.send("Welcome to the phonebook api server")
@@ -48,9 +22,7 @@ Person.find({})
     res.send(`
     <p>Phonebook has info for ${result.length} people</p>
     <p>${date}</p>`)
-
-  }
-  )
+  })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -74,10 +46,16 @@ app.get('/api/persons/:id',(req,res)=> {
 })
 
 app.delete('/api/persons/:id', (req,res) => {
+  const id = req.params.id
 
-  const id = Number(req.params.id)
-  persons = persons.filter(p=> p.id !== id)
-  res.status(204).send()
+  Person.findByIdAndRemove(id)
+  .then(result =>{
+    res.status(204).end()
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).end()
+  })
 })
 
 app.post('/api/persons',(req,res) => {
